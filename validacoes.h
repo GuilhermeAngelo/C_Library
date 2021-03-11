0f93a5704b25bc7b35754e21697b88c6be0f633e
GuilhermeAngelo <guilhermeangelo2001@gmail.com>
2021-03-11 15:09:31 -0300

    Projeto 7/15

--- validacoes.h
+++ validacoes.h
@@ -1,6 +1,12 @@
+//Recebe uma string composta apenas por números e valida se é um cpf. Se for retorna 1, caso contrário retorna 0.
 int validaCPF(char[]);
+//Recebe uma string composta letras sem caracteres especiais e valida se é um nome. Se for retorna 1, caso contrário retorna 0.
 int validaNome(char[]);
+//Recebe uma string composta letras sem caracteres especiais e valida se é um email. Se for retorna 1, caso contrário retorna 0.
 int validaEmail(char[]);
+//Recebe uma string composta letras sem caracteres especiais e núemeros ou apenas letras e valida se é um nome. Se for retorna 1, caso contrário retorna 0.
 int validaUsuario(char[]);
+//Recebe uma string composta letras sem caracteres especiais e núemeros ou apenas letras e valida se é uma senha válida. Se for retorna 1, caso contrário retorna 0.
 int validaSenha(char[]);
+//Recebe uma string composta letras sem caracteres especiais e valida se é um nome. Se for retorna 1, caso contrário retorna 0.
 int validaAutor(char[]);
